1a7ad10ccc0efe811ae48c2b7c41221d4e7b3f16
rubangarmon <rubangarmon99@gmail.com>
2022-09-10 22:59:18 -0600

    main another change 3 commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => (
   <nav>
     <section>
-      <Link to="/">Dashboard</Link>
+      <Link to="/">Dashboard something more</Link>
       <Link to="/posts">Posts--</Link>
     </section>
   </nav>
