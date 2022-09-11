93ea162f1457774dfd601fb94a290b321b637d32
rubangarmon <rubangarmon99@gmail.com>
2022-09-11 00:01:30 -0600

    feature-a first commit after merge

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export const Post = ({ post, excerpt }) => (
   <article className={excerpt ? 'post-excerpt' : 'post'}>
     <h2>{post.title}</h2>
+    <h2>{post.title}</h2>
     <p>{excerpt ? post.body.substring(0, 100) : post.body}</p>
 
     {excerpt && (
