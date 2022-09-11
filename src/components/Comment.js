869ac18448f8110c379271fc02718702c4aa6e14
rubangarmon <rubangarmon99@gmail.com>
2022-09-11 13:49:38 -0600

    feature-c commit 3

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Comment = ({ comment }) => (
 
   <aside className="comment">
-    <label>hola 1 2</label>
+    <label>hola 1 2 3</label>
     <h2>{comment.title}</h2>
     <h3>{comment.email}</h3>
     <p>{comment.body}</p>
