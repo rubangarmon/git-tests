d1d8d5b8b371234cf4e0fe1bbe3c24440a43dede
rubangarmon <rubangarmon99@gmail.com>
2022-09-10 23:50:34 -0600

    feature-b commit 4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => (
   <nav>
-    <h4>Hello</h4>
+    <h4>Hello 1</h4>
     <section>
       <Link to="/">Dashboard something more</Link>
       <Link to="/posts">Posts-- Anake</Link>
